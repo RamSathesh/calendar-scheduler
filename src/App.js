898ae2865f4fa8bff9eaa53d7d33e74f0afa16c2
RamSathesh <ramsathesh12@gmail.com>
2025-01-06 12:47:23 +0530

    FirstCommit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Overview from "./component/timeline-resources";
+// import EventScheduler from "./component/EventCalendar";
+import  EventScheduler from "./component/Event012";
+
+function App() {
+  return (
+    <div className="App">
+      <EventScheduler />
+    </div>
+  );
+}
+
+export default App;
